5be58dafdc28ca6412db1e6344407bd2ed4cb5d6
XcelevatorLavanya <Lavanya.2515@xcelevate.org>
2025-07-23 19:43:12 +0530

    Print Birth Date in Order

---
+++ FCP001.java
@@ -0,0 +1,10 @@
+public class FCP001{
+public static void main(String[]args){
+int date=22;
+String month="May";
+int year=2001;
+System.out.println(date);
+System.out.println(month);
+System.out.println(year);
+}}
+
