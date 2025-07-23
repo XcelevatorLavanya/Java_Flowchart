29836e4e65cd72afaf6b8b7807f408bb3d33a259
XcelevatorLavanya <Lavanya.2515@xcelevate.org>
2025-07-23 19:56:26 +0530

    Basic Arithmatic Ops

---
+++ FCP004.java
@@ -0,0 +1,12 @@
+public class FCP004{
+public static void main(String[]args){
+int a=12;
+int b=5;
+double div = (double)a/b;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(div);
+System.out.println(a/b);
+System.out.println(a%b);
+}}
