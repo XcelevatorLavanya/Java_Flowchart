5ce629954bb241c072315d301d05f1d7427c482c
XcelevatorLavanya <Lavanya.2515@xcelevate.org>
2025-07-30 11:51:29 +0530

    Update FCP001.java

--- FCP001.java
+++ FCP001.java
@@ -1,10 +1,14 @@
+import java.util.*;
 public class FCP001{
 public static void main(String[]args){
-int date=22;
-String month="May";
-int year=2001;
+Scanner sc=new Scanner(System.in);
+int date=sc.nextInt();
+sc.nextLine();
+String month=sc.nextLine();
+int year=sc.nextInt();
 System.out.println(date);
 System.out.println(month);
 System.out.println(year);
 }}
+
 
