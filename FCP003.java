47470312374d25ace779336d385e29e6b4d5421c
XcelevatorLavanya <Lavanya.2515@xcelevate.org>
2025-08-02 14:08:31 +0530

    Update FCP003.java

--- FCP003.java
+++ FCP003.java
@@ -1,7 +1,9 @@
+import java.util.*;
 public class FCP003{
 public static void main(String[]args){
-int a=12 ;
-int b=4;
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt() ;
+int b=sc.nextInt();
 int c=0;
 c=b;
 b=a;
