72b2e69cca3235a347ef36d529c02dc725be7780
XcelevatorLavanya <Lavanya.2515@xcelevate.org>
2025-08-02 14:41:19 +0530

    Smallest Multiple of 2 & n

---
+++ FCP013.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class FCP013{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a%2==0){
+System.out.println(a);}
+ else{
+System.out.println(a*2);}
+}}
