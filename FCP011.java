3ebbca2cfae7cd8d4a0a65837549e2300c8553e5
XcelevatorLavanya <Lavanya.2515@xcelevate.org>
2025-08-02 14:37:58 +0530

    Odd or Even Check

---
+++ FCP011.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class FCP011{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a%2==0){
+System.out.println("Even");}
+else{
+System.out.println("odd");}
+}}
